b8f5b4a24f1da0a593d84572104e1618fc26dfa4
stankoris <risimicstanko@gmail.com>
2024-07-09 17:27:50 +0200

    dodato dugme za unos

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,82 +1,62 @@
 const express = require('express');
-const app = express();
-const { Sequelize, DataTypes } = require('sequelize');
+const bodyParser = require('body-parser');
 const cors = require('cors');
+const mariadb = require('mariadb');
 
-// Dodaj CORS middleware
-app.use(cors());
-
-// Podesi konekciju sa bazom podataka
-const sequelize = new Sequelize('todo_app', 'stanko', 'stanko98', {
-    host: 'localhost',
-    dialect: 'mariadb'
-  });
-  
-// Proveri konekciju
-sequelize.authenticate()
-.then(() => console.log('Connected to the database.'))
-.catch(err => console.error('Unable to connect to the database:', err));
-    
-// Sinhronizacija baze podataka sa definisanim modelima
-sequelize.sync().then(() => {
-    console.log('Database synchronized.');
-  }).catch(err => {
-    console.error('Error synchronizing the database:', err);
-  });
-
-// Definisanje modela Todo
-const Todo = sequelize.define('todos', {
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    completed: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-    }
-  }, {
-    timestamps: true
-  });
-
-// Dodavanje rute za čitanje svih Todo stavki
-// app.get('/API/todos', async (req, res) => {
-//     try {
-//       const todos = await Todo.findAll();
-//       res.status(200).json(todos);
-//     } catch (err) {
-//       console.error('Error fetching todos:', err);
-//       res.status(500).json({ error: 'Error fetching todos' });
-//     }
-//   });
-
-
-
-const { QueryTypes } = require('sequelize');
-
-
-
-app.get('/API/todos', async (req, res) => {
-  try {
-    const todos = await sequelize.query('SELECT * FROM todos AS todo', {
-      type: QueryTypes.SELECT
-    });
-    console.log(todos);
-    res.status(200).json(todos); // Slanje rezultata kao JSON odgovor na GET zahtev
-  } catch (err) {
-    console.error('Error executing raw SQL query:', err);
-    res.status(500).json({ error: 'Error executing raw SQL query' }); // Slanje greške kao JSON odgovor na GET zahtev
-  }
-});
-
-
-
-
-
-
-
-
+const app = express();
 const port = 3001;
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
+});
+
+
+app.use(cors());
+app.use(bodyParser.json());
+
+//Pravimo konekciju sa bazom
+const pool = mariadb.createPool({
+  host: 'localhost',
+  user: 'stanko',
+  password: 'stanko98',
+  database: 'todo_app',
+  connectionLimit: 5
+});
+
+// Testiraj konekciju sa bazom
+pool.getConnection()
+  .then(conn => {
+    console.log("Connected to MariaDB!");
+    conn.release();
+  })
+  .catch(err => {
+    console.log("Not connected due to error: " + err);
+  });
+
+// Dohvatamo elemente
+app.get('/api/tasks', async (req, res) => {
+  try {
+    const conn = await pool.getConnection();
+    const tasks = await conn.query('SELECT * FROM todos');
+    conn.release();
+    res.status(200).send(tasks);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+//Dodavanje elemenata
+app.post('/api/tasks', async (req, res) => {
+  const { title } = req.body;
+  if (!title) {
+    return res.status(400).send('Title is required');
+  }
+  
+  try {
+    const conn = await pool.getConnection();
+    const result = await conn.query('INSERT INTO `todos` (`id`, `title`, `completed`, `created_at`) VALUES (NULL, ?, 0, current_timestamp());', [title]);
+    conn.release();
+    res.status(201).send({ id: result.insertId, title });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
